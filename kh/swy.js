0cb27f094483dce110ba68e575956fde2ed42db1
kh230821all362 <142883708+kh230821all362@users.noreply.github.com>
2023-11-07 16:31:18 +0900

    Update swy.js

--- kh/swy.js
+++ kh/swy.js
@@ -1,3 +1,10 @@
 function hello(){
     console.log("안녕하세요");
 }
+
+function zzz(){
+    const result = new Date('2023-11-07 17:51:00') - new Date();
+    const sec = Math.floor(result / 1000);
+    const min = Math.floor(sec / 60);
+    console.log(`퇴근 ${min}분(${sec}초) 전 ...`);
+}
